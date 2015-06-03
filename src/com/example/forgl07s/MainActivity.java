b79b3ca25fc72cfcd18dddcb1f83e079830e04af
shun <shun-fz@rontech.co.jp>
2015-06-03 16:05:34 +0900

    Create

---
+++ src/com/example/forgl07s/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.forgl07s;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+
+	 private WebView webView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		findViews(); // Viewの読み込み
+		webView.loadUrl("192.168.1.37:9000"); // サイトの読み込み
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d( "SkeletonActivity", "onResume" );
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.d( "SkeletonActivity", "onPause" );
+    }
+
+    public void findViews() {
+  	  // main.xmlのwebviewのIDを取得し、
+  	  // WebViewActivityのWebViewクラスに関連付ける
+  	  webView = (WebView)findViewById(R.id.webview);
+  	}
+  }
